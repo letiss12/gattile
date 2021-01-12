90c7593fdb1fd97228ff87acd9d998ea27838f85
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-12 19:24:56 +0100

    Create vistaAdozioni.php

---
+++ vistaAdozioni.php
@@ -0,0 +1,54 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . "dbConnection.php";
+use DB\DBAccess;
+
+$paginaHTML = file_get_contents('adozioni.html');
+
+$dbAccess = new dbAccess();
+$connessioneRiuscita = $dbAccess->openDBConnection();
+
+if ($connessioneRiuscita == false) {
+    die ("C'è stato un errore durante l'apertura del database");
+    // si chiude senza dare una risposta all'utente fare un try catch
+} else {
+    $listaG = $dbAccess->getListaGatti();
+    $dbAccess->closeDBConnection();
+
+    //definizione lista di gatti
+    $defGatti = "";
+
+    if ($listaG != null) {
+       
+        $defVolontari = '<dl id="descrizioneGatti">';
+
+        foreach ($listaG as $gatto) {
+
+            $checkGenere = '';
+            if ($gatto['Genere'] == 1) {
+                $checkGenere = 'Femmina';
+            } else if ($gatto['Genere'] == 0) {
+                $checkGenere = 'Maschio';
+            }
+
+            $defGatti .= '<dt>'. $gatto['Nome'] . '</dt>';
+            $defGatti .= '<dd>';
+            $defGatti .= '<img src="immagini'. DIRECTORY_SEPARATOR. $gatto['NomeImm'] . '" alt="' . $gatto['AltImm'] . '" />';
+            $defGatti .= '<p>' . $gatto['Genere'] . '</p>';
+            $defGatti .= '<p>' . $gatto['Descrizione'] . '</p>';
+            $defGatti .= '</dd>';
+            
+        }
+
+        $defGatti = $defGatti . "</dl>";
+
+    }
+    else {
+       
+        $defGatti = "<p>Non ci sono gatti al momento</p>";
+    }
+
+    echo str_replace("<elencoGatti />", $defGatti, $paginaHTML);
+}
+
+
+?>
