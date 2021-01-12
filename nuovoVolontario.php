b70ea5a063921b6e344c5d8995ae06c8017eaafe
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-12 19:24:53 +0100

    Update nuovoVolontario.php

--- nuovoVolontario.php
+++ nuovoVolontario.php
@@ -33,8 +33,8 @@ if (isset($_POST['submit'])) {
     $ore = $_POST['oreVol'];
     $motivazione = $_POST['motivazione'];
 
-    // CONTROLLI DA MODIFICARE, TROPPO BANALI
-    if (strlen($nome) != 0 && strlen($cognome) != 0 && strlen($dataNascita) != 0 && strlen($citta) != 0 && is_numeric($telefono) && is_numeric($ore) && strlen($motivazione) > 30 ) {
+    
+    if (strlen($nome) < 2 && strlen($cognome) < 2 && strlen($dataNascita) != 10 && strlen($citta) < 2 && is_numeric($telefono) && strlen($telefono) < 9 && is_numeric($ore) && strlen($ore) != 0 && strlen($motivazione) > 30 ) {
         // inserisco info nel database
         $dbAccess = new DBAccess();
         $openDBConnection = $dbAccess->openDBConnection();
@@ -44,7 +44,6 @@ if (isset($_POST['submit'])) {
         if($risultatoInserimento == false){
             $messaggioPerForm = '<div id="errori"><p>Si è verificato un errore nell\'invio della tua richiesta. Riprova per favore.</p></div>';
         } else if ($risultatoInserimento == true)  {
-            $messaggioPerForm = '<div id="errori"><p>Si è verificato un errore nell\'invio della tua richiesta. Riprova per favore.</p></div>';
             $messaggioPerForm = '<div id="inserito"><p>La tua richiesta è stata inviata correttamente, un sentito grazie da parte dello staff e di tutti i gatti!</p></div>';
             $nome = ''; $cognome = ''; $dataNascita = ''; $citta = ''; $telefono = ''; $volontario = ''; $animali = ''; $ore = ''; $motavazione = '';
         }
@@ -59,7 +58,7 @@ if (isset($_POST['submit'])) {
             $messaggioPerForm .= '<li>Il cognome inserito è troppo corto</li>';
         }
         if (strlen($dataNascita) != 10) {
-            $messaggioPerForm .= '<li>La data di nascita è stata inserita in modo errato, la forma corretta è gg/mm/aaaa</li>';
+            $messaggioPerForm .= '<li>La data di nascita è stata inserita in modo errato, la forma corretta è GG/MM/AAAA</li>';
         }
         if (strlen($citta) < 2) {
             $messaggioPerForm .= '<li>Il nome della città inserita è troppo corto</li>';
@@ -67,11 +66,17 @@ if (isset($_POST['submit'])) {
         if (strlen($telefono) < 9) {
             $messaggioPerForm .= '<li>Il numero di telefono inserito è troppo corto</li>';
         }
+        if (!is_numeric($telefono)) {
+            $messaggioPerForm .= '<li>Inserire il numero di telefono in formato numerico</li>';
+        }
+        if (strlen($ore) == 0) {
+            $messaggioPerForm .= '<li>Inserire una quantità di ore maggiore di zero</li>';
+        }
         if (!is_numeric($ore)) {
             $messaggioPerForm .= '<li>Inserire le ore in formato numerico</li>';
         }
         if (strlen($motivazione) < 30) {
-            $messaggioPerForm .= '<li>La motivazione deve essere di almeno 30 caratteri</li>';
+            $messaggioPerForm .= '<li>La tua spiegazione deve essere di almeno 30 caratteri</li>';
         }
 
         $messaggioPerForm .= '</ul></div>';
