d59dfc4152728b57fe4808836c33bd00da59dd0b
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-12 11:41:12 +0100

    Update vistaVolontari.php

--- vistaVolontari.php
+++ vistaVolontari.php
@@ -38,7 +38,7 @@ if ($connessioneRiuscita == false) {
 
             $defVolontari .= '<dt>' . $volontario['Nome'] .' '. $volontario['Cognome'] . '</dt>';
             $defVolontari .= '<dd>';
-            $defVolontari .= '<p>Nato il ' . $volontario['DataNascita'] . 'e residente a ' . $volontario['Citta'] . ', numero di telefono: ' . $volontario['Telefono'] . '</p>';
+            $defVolontari .= '<p>Nato il ' . $volontario['DataNascita'] . ' e residente a ' . $volontario['Citta'] . ', numero di telefono: ' . $volontario['Telefono'] . '</p>';
             $defVolontari .= '<p>' . $checkV . ' e ' . $checkA . '. Può dedicare ' . $volontario['Ore'] . ' alla settimana al volontariato.</p>';
             $defVolontari .= '<q>' . $volontario['Motivazione'] . '</q>';
             $defVolontari .= '</dd>';
