ff712e00464a6a17b13dc8123e0cf7aafb900b41
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-14 18:26:28 +0100

    Update nuovoGatto.php

--- nuovoGatto.php
+++ nuovoGatto.php
@@ -20,9 +20,36 @@ if (isset($_POST['submit'])) {
     } else if ($adozione == 'no') {
         $adozione = 0;
     }*/
+    $imm = ''; $altImm = '';
 
 
+    if (strlen($nome) > 2 && strlen($descrizione) > 10) {
+        $dbAccess = new DBAccess();
+        $openDBConnection = $dbAccess->openDBConnection();
+        $risultatoInserimento = $dbAccess->inserisciGatto($nome, $genere, $adozione, $descrizione, $imm, $altImm);
+
+        if($risultatoInserimento == false){
+            $messaggioPerForm = '<div id="errori"><p>Si è verificato un errore nella registrazione del gatto, riprova per favore.</p></div>';
+        } else if ($risultatoInserimento == true)  {
+            $messaggioPerForm = '<div id="inserito"><p>Un nuovo piccolo felino è stato registrato al rifugio!</p></div>';
+            $nome = ''; $descrizione = '';
+        }
+    }
+    else {
+        $messaggioPerForm = '<div id="errori"><ul>';
+        if (strlen($nome) <= 2) {
+            $messaggioPerForm .= '<li>Il nome del gatto è troppo corto per essere inserito</li>';
+        }
+        if (strlen($descrizione) <= 10) {
+            $messaggioPerForm .= '<li>Inserire una descrizione di almeno 10 caratteri per il gatto</li>';
+        }
+        $messaggioPerForm .= '</ul></div>';
+    }
 
 }
+
+$paginaHTML = str_replace('<messaggiForm />', $messaggioPerForm, $paginaHTML);
+$paginaHTML = str_replace('<valoreNome />', $nome, $paginaHTML);
+$paginaHTML = str_replace('<valoreDescr />', $descrizione, $paginaHTML);
 
 ?>
