8567474264390746eccbf8fecd0e56c633be4759
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-16 18:58:20 +0100

    Create eliminaGatto.php

---
+++ eliminaGatto.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . "dbConnection.php";
+use DB\DBAccess;
+
+$pagina = file_get_contents('formEliminaGatto.php');
+
+$messaggioPerForm = '';
+
+if (isset($_POST['final_delete'])) {
+    $dbAccess = new DBAccess();
+    $openDBConnection = $dbAccess->openDBConnection();
+    if (isset($_POST['delete'])) {
+        foreach ($_POST['delete'] as $deleteID) {
+            $risultato = $dbAccess->eliminaGatto($deleteID);
+        }
+    }
+    $dbAccess->closeDBConnection();
+
+    if($risultato == false) {
+        $messaggioPerForm = '<div id="errori"><p>Si è verificato un errore.</p></div>';
+    } else if ($risultato == true) {
+        $messaggioPerForm = '<div id="inserito"><p>Eliminazione completata</p></div>';
+    }
+    
+}
+
+$pagina = str_replace('<messaggiForm />', $messaggioPerForm, $pagina);
+
+echo $pagina;
+
+?>
