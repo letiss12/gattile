9388728fd3bf4b10fb94cd2d083333ee59713189
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-16 18:58:22 +0100

    Create formEliminaGatto.php

---
+++ formEliminaGatto.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . "dbConnection.php";
+use DB\DBAccess;
+
+$paginaHTML = file_get_contents('eliminaGatto.html');
+
+$dbAccess = new dbAccess();
+$connessioneRiuscita = $dbAccess->openDBConnection();
+
+if ($connessioneRiuscita == false) {
+    die ("C'è stato un errore durante l'apertura del database");
+    // si chiude senza dare una risposta all'utente fare un try catch
+} else {
+    $listaGatti = $dbAccess->getListaGatti();
+    $dbAccess->closeDBConnection();
+
+    $defForm = '';
+    if ($listaGatti != null) {
+
+        $defForm = '<fieldset>';
+
+        while ($gatto = mysqli_fetch_array($listaGatti)) {
+            $ID = $gatto['ID'];
+            $nome = $gatto['nome'];
+            $defForm .= '<input type="checkbox" name="delete[]" value="' . $ID . '" id="' . $nome . '"/>';
+            $defForm .= '<label for="' . $nome . '">' . $nome . '</label>';
+        }
+
+        $defForm .= '</fieldset>';
+    }
+
+}
+
+echo str_replace("<valoreForm />", $defForm, $paginaHTML);
+
+?>
