213aa4de06dd52647500e2e8d09803eaaf47a5b5
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-21 11:54:02 +0100

    Update nuovoGatto.php

--- nuovoGatto.php
+++ nuovoGatto.php
@@ -20,31 +20,37 @@ if (isset($_POST['submit'])) {
     } else if ($adozione == 'no') {
         $adozione = 0;
     }*/
-    $imm = ''; $altImm = '';
+    $imm = ''; 
+    $dbAccess = new DBAccess();
+    $connessioneRiuscita = $dbAccess->openDBConnection();
 
+    if ($connessioneRiuscita == false) {
+        die ("C'è stato un errore durante l'apertura del database");
+    } else {
 
-    if (strlen($nome) > 2 && strlen($descrizione) > 10) {
-        $dbAccess = new DBAccess();
-        $openDBConnection = $dbAccess->openDBConnection();
-        $risultatoInserimento = $dbAccess->inserisciGatto($nome, $genere, $adozione, $descrizione, $imm);
-        $dbAccess->closeDBConnection();
+        if (strlen($nome) > 2 && strlen($descrizione) > 10) {
+            $risultatoInserimento = $dbAccess->inserisciGatto($nome, $genere, $adozione, $descrizione, $imm);
+            $dbAccess->closeDBConnection();
 
-        if($risultatoInserimento == false){
-            $messaggioPerForm = '<div id="errori"><p>Si è verificato un errore nella registrazione del gatto, riprova per favore.</p></div>';
-        } else if ($risultatoInserimento == true)  {
-            $messaggioPerForm = '<div id="inserito"><p>Un nuovo piccolo felino è stato registrato al rifugio!</p></div>';
-            $nome = ''; $descrizione = '';
+            if($risultatoInserimento == false){
+                $messaggioPerForm = '<div class="messForm id="errore"><p>Si è verificato un errore nella registrazione del gatto, riprova per favore.</p></div>';
+            } else if ($risultatoInserimento == true)  {
+                $messaggioPerForm = '<div class="messForm id="completato"><p>Un nuovo piccolo felino è stato registrato al rifugio!</p></div>';
+                $nome = ''; $descrizione = '';
+            }
+
+        } else {
+            $dbAccess->closeDBConnection();
+            $messaggioPerForm = '<div class="messForm id="errore"><ul>';
+            if (strlen($nome) <= 2) {
+                $messaggioPerForm .= '<li>Il nome del gatto è troppo corto per essere inserito</li>';
+            }
+            if (strlen($descrizione) <= 10) {
+                $messaggioPerForm .= '<li>Inserire una descrizione di almeno 10 caratteri per il gatto</li>';
+            }
+            $messaggioPerForm .= '</ul></div>';
         }
-    }
-    else {
-        $messaggioPerForm = '<div id="errori"><ul>';
-        if (strlen($nome) <= 2) {
-            $messaggioPerForm .= '<li>Il nome del gatto è troppo corto per essere inserito</li>';
-        }
-        if (strlen($descrizione) <= 10) {
-            $messaggioPerForm .= '<li>Inserire una descrizione di almeno 10 caratteri per il gatto</li>';
-        }
-        $messaggioPerForm .= '</ul></div>';
+
     }
 
 }
