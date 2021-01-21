4bda9920149e940dbbf5e8e0951097f32b2f49b3
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-21 18:02:08 +0100

    Update home.php

--- home.php
+++ home.php
@@ -11,14 +11,24 @@ $messIndex ='';
 if ($connessioneRiuscita == false) {
     die ("C'è stato un errore durante l'apertura del database");
 } else {
-    $numGatti = $dbAccess->getNumAdottati();
-    //$listaG = $dbAccess->getListaGatti();
+    $listaG = $dbAccess->getListaGatti();
     $dbAccess->closeDBConnection();
 
-    $messIndex = '<p> Già '. $numGatti . ' del rifugio hanno trovato una casa! Cosa aspetti?';
+    if ($listaG != null) {
+
+        $messIndex = '<p>Questi bellissimi gatti hanno già trovato una casa! Tu cosa aspetti?</p><dl>';
+        foreach ($listaG as $gatto) {
+            if ($gatto['Adozione'] == 1) {
+                $messIndex .= '<dt>'. $gatto['Nome'] .'</dt>';
+                $messIndex .= '<dd><img src="immagini'. DIRECTORY_SEPARATOR. 'gatti'. DIRECTORY_SEPARATOR. $gatto['NomeImm'] . '" alt="' . $gatto['AltImm'] . '" /></dd>';
+            }
+            $messIndex .= '</dl>';
+
+        }
+    }
 
 
-
+   
 }
 
 str_replace("<contatoreGatti />", $messIndex, $paginaHTML);
