ae26803276cb6de7d294e709c8d62181ba9efb2f
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-21 11:54:07 +0100

    Update nuovoVolontario.php

--- nuovoVolontario.php
+++ nuovoVolontario.php
@@ -33,54 +33,59 @@ if (isset($_POST['submit'])) {
     $ore = $_POST['oreVol'];
     $motivazione = $_POST['motivazione'];
 
-    
-    if (strlen($nome) >= 2 && strlen($cognome) >= 2 && strlen($dataNascita) != 10 && strlen($citta) >= 2 && is_numeric($telefono) && strlen($telefono) >= 9 && is_numeric($ore) && strlen($ore) != 0 && strlen($motivazione) > 30 ) {
-        // inserisco info nel database
-        $dbAccess = new DBAccess();
-        $openDBConnection = $dbAccess->openDBConnection();
-        $risultatoInserimento = $dbAccess->inserisciVolontario($nome, $cognome, $dataNascita, $citta, $telefono, $volontario, $animali, $ore, $motivazione);
-        $dbAccess->closeDBConnection();
+    $dbAccess = new DBAccess();
+    $connessioneRiuscita = $dbAccess->openDBConnection();
 
-        if($risultatoInserimento == false){
-            $messaggioPerForm = '<div id="errori"><p>Si è verificato un errore nell\'invio della tua richiesta. Riprova per favore.</p></div>';
-        } else if ($risultatoInserimento == true)  {
-            $messaggioPerForm = '<div id="inserito"><p>La tua richiesta è stata inviata correttamente, un sentito grazie da parte dello staff e di tutti i gatti!</p></div>';
-            $nome = ''; $cognome = ''; $dataNascita = ''; $citta = ''; $telefono = ''; $volontario = ''; $animali = ''; $ore = ''; $motavazione = '';
-        }
-
+    if ($connessioneRiuscita == false) {
+        die ("C'è stato un errore durante l'apertura del database");
     } else {
-        // messaggi di errore
-        $messaggioPerForm = '<div id="errori"><ul>';
-        if (strlen($nome) < 2) {
-            $messaggioPerForm .= '<li>Il nome inserito è troppo corto</li>';
-        }
-        if (strlen($cognome) < 2) {
-            $messaggioPerForm .= '<li>Il cognome inserito è troppo corto</li>';
-        }
-        if (strlen($dataNascita) != 10) {
-            $messaggioPerForm .= '<li>La data di nascita è stata inserita in modo errato, la forma corretta è GG/MM/AAAA</li>';
-        }
-        if (strlen($citta) < 2) {
-            $messaggioPerForm .= '<li>Il nome della città inserita è troppo corto</li>';
-        }
-        if (strlen($telefono) < 9) {
-            $messaggioPerForm .= '<li>Il numero di telefono inserito è troppo corto</li>';
-        }
-        if (!is_numeric($telefono)) {
-            $messaggioPerForm .= '<li>Inserire il numero di telefono in formato numerico</li>';
-        }
-        if (strlen($ore) == 0) {
-            $messaggioPerForm .= '<li>Inserire una quantità di ore maggiore di zero</li>';
-        }
-        if (!is_numeric($ore)) {
-            $messaggioPerForm .= '<li>Inserire le ore in formato numerico</li>';
-        }
-        if (strlen($motivazione) < 30) {
-            $messaggioPerForm .= '<li>La tua spiegazione deve essere di almeno 30 caratteri</li>';
-        }
 
-        $messaggioPerForm .= '</ul></div>';
+        if (strlen($nome) >= 2 && strlen($cognome) >= 2 && strlen($dataNascita) != 10 && strlen($citta) >= 2 && is_numeric($telefono) && strlen($telefono) >= 9 && is_numeric($ore) && strlen($ore) != 0 && strlen($motivazione) > 30 ) {
+        
+            $risultatoInserimento = $dbAccess->inserisciVolontario($nome, $cognome, $dataNascita, $citta, $telefono, $volontario, $animali, $ore, $motivazione);
+            $dbAccess->closeDBConnection();
 
+            if($risultatoInserimento == false){
+                $messaggioPerForm = '<div class="messForm" id="errore"><p>Si è verificato un errore nell\'invio della tua richiesta. Riprova per favore.</p></div>';
+            } else if ($risultatoInserimento == true)  {
+                $messaggioPerForm = '<div class="messForm id="completato"><p>La tua richiesta è stata inviata correttamente, un sentito grazie da parte dello staff e di tutti i gatti!</p></div>';
+                $nome = ''; $cognome = ''; $dataNascita = ''; $citta = ''; $telefono = ''; $volontario = ''; $animali = ''; $ore = ''; $motavazione = '';
+            }
+
+        } else {
+            $dbAccess->closeDBConnection();
+            // messaggi di errore
+            $messaggioPerForm = '<div class="messForm id="errore"><ul>';
+            if (strlen($nome) < 2) {
+                $messaggioPerForm .= '<li>Il nome inserito è troppo corto</li>';
+            }
+            if (strlen($cognome) < 2) {
+                $messaggioPerForm .= '<li>Il cognome inserito è troppo corto</li>';
+            }
+            if (strlen($dataNascita) != 10) {
+                $messaggioPerForm .= '<li>La data di nascita è stata inserita in modo errato, la forma corretta è GG/MM/AAAA</li>';
+            }
+            if (strlen($citta) < 2) {
+                $messaggioPerForm .= '<li>Il nome della città inserita è troppo corto</li>';
+            }
+            if (strlen($telefono) < 9) {
+                $messaggioPerForm .= '<li>Il numero di telefono inserito è troppo corto</li>';
+            }
+            if (!is_numeric($telefono)) {
+                $messaggioPerForm .= '<li>Inserire il numero di telefono in formato numerico</li>';
+            }
+            if (strlen($ore) == 0) {
+                $messaggioPerForm .= '<li>Inserire una quantità di ore maggiore di zero</li>';
+            }
+            if (!is_numeric($ore)) {
+                $messaggioPerForm .= '<li>Inserire le ore in formato numerico</li>';
+            }
+            if (strlen($motivazione) < 30) {
+                $messaggioPerForm .= '<li>La tua spiegazione deve essere di almeno 30 caratteri</li>';
+            }
+
+            $messaggioPerForm .= '</ul></div>';
+        }
     }
 }
 
